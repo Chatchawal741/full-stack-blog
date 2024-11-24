ac0555f9dc016b822da30fbcf990927c47987d3e
Chatchawal <132872971+Chatchawal741@users.noreply.github.com>
2024-11-24 18:38:59 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// components
+import Homepage from "./Routes/Homepage.jsx";
+import PostListPage from "./Routes/PostListPage.jsx";
+import Login from "./Routes/Login.jsx";
+import Register from "./Routes/Register.jsx";
+import Write from "./Routes/Write.jsx";
+import SinglePostPage from "./Routes/SinglePostPage.jsx";
+import MainLayout from "./Layout/MainLayout.jsx";
+// router-dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// import clerk provider
+import { ClerkProvider } from "@clerk/clerk-react";
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// check if publishable key exists
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+// router
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
