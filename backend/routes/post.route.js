276d9907f3e5e1d88883666e7c779529fa7802f8
CHATL <chatchawal2547@gmail.com>
2024-12-01 01:46:38 +0700

    Update post.route.js

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-
+import {
+  getPosts,
+  getPost,
+  createPost,
+  deletePost,
+} from "../controller/post.controller.js";
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send("Post route");
-});
-
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
 export default router;
