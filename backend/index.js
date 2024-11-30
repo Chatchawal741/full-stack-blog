e4d20c75c4047b8d58173ada5680461b2126432a
CHATL <chatchawal2547@gmail.com>
2024-12-01 01:44:38 +0700

    new error handling for express5
    new error handling for express5 and exclude webhook to middleware

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,13 @@ import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import connectDB from "./lib/connectDB.js";
+import clerkRouter from "./routes/webhook.route.js";
 const app = express();
+
+// Exclude webhook from express's middleware
+app.use("/webhooks", clerkRouter);
+// Middleware
+app.use(express.json());
 const PORT = 3000;
 
 // node watch
@@ -16,9 +22,19 @@ const PORT = 3000;
 app.use("/users", userRouter);
 app.use("/post", postRouter);
 app.use("/comment", commentRouter);
-console.log("hello");
 
-app.listen(PORT, () => {
-  connectDB();
+// new handling error in expres@5
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: error.message || "Something went Wrong",
+    status: error.status || 500,
+    stack: error.stack,
+  });
+});
+
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
