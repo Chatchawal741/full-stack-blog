5cb5b2576a62bc3eb23618ca0a0cf81b5a429030
CHATL <chatchawal2547@gmail.com>
2024-12-12 00:23:56 +0700

    Update PostList.jsx
    adding useQuery for managing form
    adding new request for form react-query

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,14 +1,25 @@
-import React from "react";
 import PostListItems from "./PostListItem";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPost = async () => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/posts`);
+  return await res.json();
+};
 
 function PostList() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () => fetchPost(),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log(data);
+
   return (
     <div className="flex flex-col gap-12 mb-8">
-      <PostListItems />
-      <PostListItems />
-      <PostListItems />
-      <PostListItems />
-      <PostListItems />
       <PostListItems />
     </div>
   );
